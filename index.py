54784972f0fd71f1d4ee70b01ce1a7dc85a2eeb7
Vishal <82800646+LedaupSivlah@users.noreply.github.com>
2021-10-09 19:04:14 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,6 +7,19 @@ print('Logging in...')
 client = commands.Bot(command_prefix='plaksha ', intents=discord.Intents(guilds=True, members=True, messages=True, reactions=True, presences=True))
 print('Logged in!   ')
 
+Upskill_Dict =
+{
+  'Leader_Board' :
+    {
+      'username': 'number_of_praises',
+    },
+
+  'tags' :
+    {
+      'Name_of_Tag': ['contents/links'],
+    },
+}
+
 
 
 @client.command()
@@ -26,20 +39,28 @@ async def upskill(ctx):
   pass
 
 
-@upskill.command(name='tag', invoke_without_command=True)
+@upskill.group(aliases=['tag', 'fav', 'push'], invoke_without_command=True)
 async def tag(ctx):
   if ctx.message.channel.category.id == 850043220020559933:
     if ctx.message.reference:
-      await ctx.message.reply('This?')
+      if not(ctx.message.reference.fail_if_not_exists):
+        await ctx.message.reply('Sorry this message may have been deleted!')
+      else:
+        await ctx.message.reference.reply('Please define what do you want to do with this message.\n> For creating a tag of this use ```plaksha upskill tag create NAME```')
+    else:
+      await ctx.message.reply('Please reply/refer to a message while tagging it.')
+    
 
 
   else:
-    
-
     category = discord.utils.get(ctx.guild.channels, id=850043220020559933)
 
     await ctx.reply(f'{ctx.author} this should be used inside one of {category}')
 
+@tag.group(aliases=['create', '+'], invoke_without_command=True)
+async def create(ctx, name:str):
+  
+  
 
 
 def create_embed():
