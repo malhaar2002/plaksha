affc6382554d4811b170399d02dd6e8f2c55819d
Vishal <82800646+LedaupSivlah@users.noreply.github.com>
2021-10-09 16:54:11 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import os
 
 
 print('Logging in...')
-client = commands.Bot(command_prefix='plaksha ', intents=discord.Intents(guilds=True, members=True, messages=True, reactions=True, presences=True))
+client = commands.Bot(command_prefix='plaksha', intents=discord.Intents(guilds=True, members=True, messages=True, reactions=True, presences=True))
 print('Logged in!   ')
 
 
@@ -18,8 +18,27 @@ async def calling(ctx):
   await ctx.send(f'Indeed, just {days} days before {commencement_date}')
   pass
 
-for filename in os.listdir('./cogs'):
-  if filename.endswith('.py'):
-    client.load_extension(f'cogs.{filename[:-3]}')
+
+
+@client.group(name='upskill', invoke_without_command=True):
+async def upskill(ctx):
+  await ctx.send(create_embed(), timeout='60')
+  pass
+
+
+@upskill.command(name='tag', invoke_without_command=True):
+ async def tag(ctx):
+  if ctx.category.id = 850043220020559933:
+    if ctx.message.reference:
+      await ctx.message.reference.reply('This?')
+
+
+
+def create_embed():
+  return 'Hm...'
+
+# for filename in os.listdir('./cogs'):
+#   if filename.endswith('.py'):
+#     client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run(os.environ['DISCORD_TOKEN'])
