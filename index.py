610a354cc2cfd6a2e0f80d84f1dc010bd7387fb4
Vishal <82800646+LedaupSivlah@users.noreply.github.com>
2021-10-09 22:19:09 +0530

    Update index.py

--- index.py
+++ index.py
@@ -72,7 +72,7 @@ async def create(ctx, *, tag_name:str=''):
         return
 
     else:
-      Upskill_Dict[tag_name] = (ctx.message.reference.resolved).content
+      Tag_Dict[tag_name] = (ctx.message.reference.resolved).content
       await ctx.message.reply(f'Tag was created! Use```plaksha upskill tag {tag_name}```To reference it in the future!!')
   else:
     await ctx.send(f'{(ctx.author).mention} Please input a NAME for the tag```plaksha upskill tag create NAME```', reference=ctx.message.reference)
