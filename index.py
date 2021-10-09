de7e5f49bb3936bf7c79c9c9db91783d247b67ea
Vishal <82800646+LedaupSivlah@users.noreply.github.com>
2021-10-09 23:40:20 +0530

    Update index.py

--- index.py
+++ index.py
@@ -64,7 +64,7 @@ async def create(ctx, *, tag_name:str=''):
 
           if tag_name in name_element_list:
 
-            await ctx.message.reply(f'{ctx.author.mention}\n> There already exists a tag with this name {tag_name}!!\nwith content\n> {Tag_Dict[tag_name]}\nIf you want to add stuff in this tag Use:```plaksha tag more-over Name```')
+            await ctx.message.reply(f'{ctx.author.mention}\n> There already exists a tag with this name {tag_name}!!\nwith content\n> {Tag_Dict[tag_name]}\nIf you want to add stuff in this tag Use:```plaksha tag more_over Name```')
             return
 
           elif (ctx.message.reference.resolved).content in content_element_list:
@@ -90,7 +90,28 @@ async def create(ctx, *, tag_name:str=''):
   else:
     await ctx.message.reply('Please reply/refer to a message while tagging it.')
 
+@tag.group(aliases=['more'], invoke_without_command=True)
+async def more_over(ctx, *, tag_name:str=''):
+  
+  if ctx.message.reference:
+    
+    if not(ctx.message.reference.fail_if_not_exists):
+      await ctx.message.reply('Sorry this message may have been deleted!')
 
+    else:
+
+      for name_element_list, content_element_list in Tag_Dict.items():
+
+        if tag_name in name_element_list:
+
+          
+          
+          Tag_Dict[name_element_list] = content_element.append(ctx.message.reference.resolved.content)
+          return
+      
+      
+  else:
+    ctx.message.reply('Please reply/refer to a message while tagging it.')
   
   
 
