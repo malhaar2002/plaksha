e61a9249ca96fa16de9f6069f0581222e64f9265
Vishal <82800646+LedaupSivlah@users.noreply.github.com>
2021-10-10 11:24:14 +0530

    Update tag.py

--- cogs/tag.py
+++ cogs/tag.py
@@ -3,8 +3,48 @@ from discord.ext import commands
 
 class Tag(commands.Cog):
 
-  def __init__(self, bot):
-    self.bot = bot
+  Tag_Dict = {'first_name':['content/links']}
+  Alias_Dict = {'first_name':['aliases']}
+
+  def __init__(self, client):
+    self.bot = client
+
+
+  def tag_search(self, name, content):
+
+
+    def content_search(self, content):
+      content_present = False
+
+      for first_name in Tag_Dict.keys():
+        for content_element in Tag_Dict['first_name']:
+          if content_element == content:
+            content_present = True
+            return content_present
+      else:
+        content_present = False
+        return content_present
+
+     def name_search(self, name):
+        name_present = False
+
+        for first_name in Alias_Dict.keys():
+          if first_name == name:
+            name_present = True
+            return name_present
+          
+          for alias_name Alias_Dict[first_name]:
+            if alias_name == name:
+              name_present = True
+              return name_present
+
+        else:
+          name_present = False
+          return name_present
+
+     content_present = content_search(name)
+      
+          
 
 
   @commands.group(aliases=['fav', 'push'], invoke_without_command=True)
@@ -20,13 +60,7 @@ class Tag(commands.Cog):
         await ctx.message.reply('Please reply/refer to a message while tagging it.')
 
     else:
-      # Add better search and nearby words later on
-      for tag_name_list in Tag_Dict.keys():
-        if tag_name in tag_name_list:
-          await ctx.send(f'{Tag_Dict[tag_name_list]}', reference=ctx.message.reference)
-          return
-      else:
-        await ctx.message.reply(f'There is no tag with this name {tag_name}\n> Use:```plaksha tag create NAME```')
+      
 
 
 
@@ -44,28 +78,7 @@ class Tag(commands.Cog):
 
         if tag_name != '':
 
-
-          for name_element_list, content_element_list in Tag_Dict.items():
-
-            if tag_name in name_element_list:
-
-              await ctx.message.reply(f'{ctx.author.mention}\n> There already exists a tag with this name {tag_name}!!\nwith content\n> {Tag_Dict[tag_name]}\nIf you want to add stuff in this tag Use:```plaksha tag more_over Name```')
-              return
-
-            elif (ctx.message.reference.resolved).content in content_element_list:
-
-              del Tag_Dict[name_element_list]
-              name_element_list.append(tag_name)
-
-              Tag_Dict[name_element_list] = content_element_list
-
-              await ctx.send(f'This content is already tagged, added an alias for this content! Use```plaksha tag {tag_name}```To reference it in the future!!', refeerence=ctx.message.reference)
-              return
-
-          else:
-            Tag_Dict[[tag_name]] = [(ctx.message.reference.resolved).content]
-            await ctx.message.reply(f'Tag was created! Use```plaksha tag {tag_name}```To reference it in the future!!')
-
+          pass
 
 
         else:
@@ -86,15 +99,7 @@ class Tag(commands.Cog):
         await ctx.message.reply('Sorry this message may have been deleted!')
 
       else:
-
-        for name_element_list, content_element_list in Tag_Dict.items():
-
-          if tag_name in name_element_list:
-
-            # CONTENT VS NAME create a function to check both at the same time
-
-            Tag_Dict[name_element_list] = content_element.append(ctx.message.reference.resolved.content)
-            return
+        return
 
 
     else:
