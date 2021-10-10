714950ba4522cdff8116671f407726e89840963d
Vishal Mac <VishalPaudel@yandex.com>
2021-10-10 11:56:48 +0530

    "fixed indentations"

--- cogs/tag.py
+++ cogs/tag.py
@@ -1,111 +1,102 @@
 import discord
 from discord.ext import commands
 
+
 class Tag(commands.Cog):
 
-  Tag_Dict = {'first_name':['content/links']}
-  Alias_Dict = {'first_name':['aliases']}
+    Tag_Dict = {'first_name': ['content/links']}
+    Alias_Dict = {'first_name': ['aliases']}
 
-  def __init__(self, client):
-    self.bot = client
+    def __init__(self, client):
+        self.bot = client
 
+    def tag_search(self, name, content):
+        global Tag_Dict, Alias_Dict
 
-  def tag_search(self, name, content):
+        def content_search(self, content):
+            content_present = False
 
+            for first_name in Tag_Dict.keys():
+                for content_element in Tag_Dict[first_name]:
+                    if content_element == content:
+                        content_present = True
+                        return content_present
+            else:
+                content_present = False
+                return content_present
 
-    def content_search(self, content):
-      content_present = False
+        def name_search(self, name):
+            name_present = False
 
-      for first_name in Tag_Dict.keys():
-        for content_element in Tag_Dict['first_name']:
-          if content_element == content:
-            content_present = True
-            return content_present
-      else:
-        content_present = False
-        return content_present
+            for first_name in Alias_Dict.keys():
+                if first_name == name:
+                    name_present = True
+                    return name_present
 
-     def name_search(self, name):
-        name_present = False
+                for alias_name in Alias_Dict[first_name]:
+                    if alias_name == name:
+                        name_present = True
+                        return name_present
 
-        for first_name in Alias_Dict.keys():
-          if first_name == name:
-            name_present = True
-            return name_present
-          
-          for alias_name Alias_Dict[first_name]:
-            if alias_name == name:
-              name_present = True
-              return name_present
+            else:
+                name_present = False
+                return name_present
 
-        else:
-          name_present = False
-          return name_present
+        content_present = content_search(name)
+        name_present = name_search(content)
 
-     content_present = content_search(name)
-      
-          
+        return content_present, name_present
 
-
-  @commands.group(aliases=['fav', 'push'], invoke_without_command=True)
-  async def tag(self, ctx, *, tag_name:str=''):
-    if tag_name == '':
-      # Send a help command here
-      if ctx.message.reference != None:
-        if not(ctx.message.reference.fail_if_not_exists):
-          await ctx.message.reply('Sorry this message may have been deleted!')
-        else:
-          await ctx.send('*Please define what do you want to do with this message.*\n> For **creating a tag** of this, Use ```plaksha tag create NAME```', reference=ctx.message.reference)
-      else:
-        await ctx.message.reply('Please reply/refer to a message while tagging it.')
-
-    else:
-      
-
-
-
-  @tag.group(aliases=['+'], invoke_without_command=True)
-  async def create(self, ctx, *, tag_name:str=''):
-
-
-    if ctx.message.reference != None:
-
-      if not(ctx.message.reference.fail_if_not_exists):
-        await ctx.message.reply('Sorry this message may have been deleted!')
-
-
-      else:
-
-        if tag_name != '':
-
-          pass
-
+    @commands.group(aliases=['fav', 'push'], invoke_without_command=True)
+    async def tag(self, ctx, *, tag_name: str = ''):
+        if tag_name == '':
+            # Send a help command here
+            if ctx.message.reference != None:
+                if not(ctx.message.reference.fail_if_not_exists):
+                    await ctx.message.reply('Sorry this message may have been deleted!')
+                else:
+                    await ctx.send('*Please define what do you want to do with this message.*\n> For **creating a tag** of this, Use ```plaksha tag create NAME```', reference=ctx.message.reference)
+            else:
+                await ctx.message.reply('Please reply/refer to a message while tagging it.')
 
         else:
-          await ctx.send(f'{(ctx.author).mention} Please input a NAME for the tag```plaksha tag create NAME```', reference=ctx.message.reference)
-        pass
 
-    else:
-      await ctx.message.reply('Please reply/refer to a message while tagging it.')
+    @tag.group(aliases=['+'], invoke_without_command=True)
+    async def create(self, ctx, *, tag_name: str = ''):
 
+        if ctx.message.reference != None:
 
+            if not(ctx.message.reference.fail_if_not_exists):
+                await ctx.message.reply('Sorry this message may have been deleted!')
 
-  @tag.group(aliases=['more'], invoke_without_command=True)
-  async def more_over(self, ctx, *, tag_name:str=''):
+            else:
 
-    if ctx.message.reference:
+                if tag_name != '':
 
-      if not(ctx.message.reference.fail_if_not_exists):
-        await ctx.message.reply('Sorry this message may have been deleted!')
+                    pass
 
-      else:
-        return
+                else:
+                    await ctx.send(f'{(ctx.author).mention} Please input a NAME for the tag```plaksha tag create NAME```', reference=ctx.message.reference)
+                pass
 
+        else:
+            await ctx.message.reply('Please reply/refer to a message while tagging it.')
 
-    else:
-      ctx.message.reply('Please reply/refer to a message while tagging it.')
+    @tag.group(aliases=['more'], invoke_without_command=True)
+    async def more_over(self, ctx, *, tag_name: str = ''):
 
+        if ctx.message.reference:
+
+            if not(ctx.message.reference.fail_if_not_exists):
+                await ctx.message.reply('Sorry this message may have been deleted!')
+
+            else:
+                return
+
+        else:
+            ctx.message.reply(
+                'Please reply/refer to a message while tagging it.')
 
 
 def setup(client):
-  client.add_cog(Tag(client))
+    client.add_cog(Tag(client))
--- index.py
+++ index.py
@@ -4,28 +4,28 @@ import os
 
 
 print('Logging in...')
-client = commands.Bot(command_prefix='plaksha ', intents=discord.Intents(guilds=True, members=True, messages=True, reactions=True, presences=True))
+client = commands.Bot(command_prefix='plaksha ', intents=discord.Intents(
+    guilds=True, members=True, messages=True, reactions=True, presences=True))
 print('Logged in!   ')
-
 
 
 @client.command()
 async def calling(ctx):
 
-  commencement_date = '09/11/2021'
-  days = "39 - today's date (october)"
-  
-  await ctx.send(f'Indeed, just {days} days before {commencement_date}')
-  pass
+    commencement_date = '09/11/2021'
+    days = "39 - today's date (october)"
+
+    await ctx.send(f'Indeed, just {days} days before {commencement_date}')
+    pass
 
 
 def create_embed():
-  return 'Hm...'
+    return 'Hm...'
 
 
 for filename in os.listdir('./cogs'):
-  if filename.endswith('.py'):
-    client.load_extension(f'cogs.{filename[:-3]}')
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 
 client.run(os.environ['DISCORD_TOKEN'])
