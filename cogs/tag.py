5edbae6af28cd324edb09e3b26f3c83499d07cbc
Vishal Mac <VishalPaudel@yandex.com>
2021-10-10 12:45:22 +0530

    indent error

--- cogs/tag.py
+++ cogs/tag.py
@@ -9,6 +9,8 @@ class Tag(commands.Cog):
 
     def __init__(self, client):
         self.bot = client
+        self.Tag_Dict = {'first_name': ['content/links']}
+        self.Alias_Dict = {'first_name': ['aliases']}
 
     def tag_search(self, name, content):
         global Tag_Dict, Alias_Dict
@@ -16,8 +18,8 @@ class Tag(commands.Cog):
         def content_search(self, content):
             content_present = False
 
-            for first_name in Tag_Dict.keys():
-                for content_element in Tag_Dict[first_name]:
+            for first_name in self.Tag_Dict.keys():
+                for content_element in self.Tag_Dict[first_name]:
                     if content_element == content:
                         content_present = True
                         return content_present
@@ -28,12 +30,12 @@ class Tag(commands.Cog):
         def name_search(self, name):
             name_present = False
 
-            for first_name in Alias_Dict.keys():
+            for first_name in self.Alias_Dict.keys():
                 if first_name == name:
                     name_present = True
                     return name_present
 
-                for alias_name in Alias_Dict[first_name]:
+                for alias_name in self.Alias_Dict[first_name]:
                     if alias_name == name:
                         name_present = True
                         return name_present
@@ -60,6 +62,9 @@ class Tag(commands.Cog):
                 await ctx.message.reply('Please reply/refer to a message while tagging it.')
 
         else:
+            content_present, name_present = tag_search(tag_name, None)
+            if name_present:
+                await ctx.reply('Tag found!')
 
     @tag.group(aliases=['+'], invoke_without_command=True)
     async def create(self, ctx, *, tag_name: str = ''):
