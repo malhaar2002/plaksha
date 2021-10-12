d4e93a7977ac85098adf036a7a78d475133403c6
Vishal Mac <VishalPaudel@yandex.com>
2021-10-12 16:32:48 +0530

    added setup in beutify

--- cogs/beautify.py
+++ cogs/beautify.py
@@ -1,2 +1,14 @@
+from discord.ext import commands
+
 # nothing here for now
 # need to make a proper embeding cog
+
+
+class Beautify(commands.Cog):
+
+    def __init__(self, client):
+        self.bot = client
+
+
+def setup(client):
+    client.add_cog(Beautify(client))
