7e8eb250f25a4539db5657339c60b87ad50b56a7
Vishal Mac <VishalPaudel@yandex.com>
2021-10-12 20:57:53 +0530

    get channels

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -14,7 +14,7 @@ class Miscellaneous(commands.Cog):
     async def on_member_join(self, member: discord.Member):
         channel = discord.utils.get(
             member.guild.get_all_channels(), id=833750512276471878)
-        await member.add_roles(get(member.guild.roles, id=897167079176818729))
+        await member.add_roles(get(member.guild.channels, id=897167079176818729))
 
         if channel is not None:
             Welcome_Embed = discord.Embed(
